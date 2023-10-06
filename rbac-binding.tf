fdf9defae8d211c50f0969c405d8c8b4b9a67a4e
nailabx <140486166+nailabx@users.noreply.github.com>
2023-10-05 20:58:33 -0500

    add role binding (#1)
    Co-authored-by: lacinazina <zinalacina@gmail.com>

--- rbac-binding.tf
+++ rbac-binding.tf
@@ -1,20 +1,54 @@
-# readonly binding
-resource "kubernetes_cluster_role_binding" "readonly" {
+resource "kubernetes_role" "default" {
   metadata {
-    name = "readonly"
+    name = "default-access"
+    namespace = "default"
   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind     = "ClusterRole"
-    name     = "view"
+  rule {
+    api_groups = [""]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+resource "kubernetes_role_binding" "full_default_access" {
+  metadata {
+    name = "default-access"
+    namespace = "default"
   }
 
   subject {
     kind      = "Group"
-    name      = "readonly"
+    name      = "default-access"
+    api_group = "rbac.authorization.k8s.io"
   }
+
+  role_ref {
+    kind     = "Role"
+    name     = kubernetes_role.default.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  depends_on = [kubernetes_role.default]
 }
+
+# readonly binding
+#resource "kubernetes_cluster_role_binding" "readonly" {
+#  metadata {
+#    name = "readonly"
+#  }
+#
+#  role_ref {
+#    api_group = "rbac.authorization.k8s.io"
+#    kind     = "ClusterRole"
+#    name     = "view"
+#  }
+#
+#  subject {
+#    kind      = "Group"
+#    name      = "readonly"
+#  }
+#}
 
 # admin binding
 resource "kubernetes_cluster_role_binding" "admin" {
